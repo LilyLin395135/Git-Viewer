20b7e4292ba910813a519ba7fc5006b05aa8f4b1
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-06-12 09:13:56 +0800

    feat: Sever is running on port

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Sever is running on port ${port}`);
+});
