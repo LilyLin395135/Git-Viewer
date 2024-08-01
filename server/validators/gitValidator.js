eafaa16afcbb60b5654de7209b2ba33e60793e13
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-08-01 17:46:01 +0800

    feat: remove no use file

--- server/validators/gitValidator.js
+++
@@ -1,9 +0,0 @@
-import { body, query, param } from 'express-validator';
-
-export const gitInitValidator = [
-  body('folderPath').notEmpty().withMessage('folderPath is required')
-];
-
-export const gitGetBranchInfoValidator = [
-  query('folderPath').notEmpty().withMessage('folderPath is required')
-];
